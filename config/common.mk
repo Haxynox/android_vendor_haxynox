62c11f5e0b7994e9d5d30aa8cf347261a955cc50
Alectradar <gokulbalram@gmail.com>
2016-04-09 20:48:01 +0530

    Include a filter

--- config/common.mk
+++ config/common.mk
@@ -1,6 +1,8 @@
 #Bootanimation
+ifneq ($(filter i9300 n7100,$(PRODUCT_DEVICE)),)
 PRODUCT_COPY_FILES += \
     vendor/tesla/prebuilt/common/media/bootanimation.zip:system/media/bootanimation.zip
+endif
 
 #Launcher
 PRODUCT_PACKAGES += \
