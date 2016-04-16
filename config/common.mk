40c79d15006470a9d90d2885f7b97b7e70c0d685
Gokul Balram <gokulbalram@gmail.com>
2016-04-17 03:15:48 +0530

    Correct the call (TARGET, not PRODUCT)

--- config/common.mk
+++ config/common.mk
@@ -1,5 +1,5 @@
 #Bootanimation
-ifneq ($(filter i9300 n7100,$(PRODUCT_DEVICE)),)
+ifneq ($(filter i9300 n7100,$(TARGET_DEVICE)),)
 PRODUCT_COPY_FILES += \
     vendor/haxynox/prebuilt/common/media/bootanimation.zip:system/media/bootanimation.zip
 endif
